2f99572bbc9524a71634ea138b009109e86adaee
Mena Azab <menaazab@hotmail.com>
2023-06-24 13:51:51 -0400

    updated metamask

--- src/Pages/StartPage.js
+++ src/Pages/StartPage.js
@@ -8,29 +8,48 @@ import detectEthereumProvider from '@metamask/detect-provider'
 
 function StartPage () {
     const [hasProvider, setHasProvider] = useState(null)
-    const initialState = { accounts: [] }               /* New */
-    const [wallet, setWallet] = useState(initialState)  /* New */
+    const initialState = { accounts: [] }               
+    const [wallet, setWallet] = useState(initialState)  
 
   useEffect(() => {
+    const refreshAccounts = (accounts: any) => {                /* New */
+      if (accounts.length > 0) {                                /* New */
+        updateWallet(accounts)                                  /* New */
+      } else {                                                  /* New */
+        // if length 0, user is disconnected                    /* New */
+        setWallet(initialState)                                 /* New */
+      }                                                         /* New */
+    } 
+
     const getProvider = async () => {
       const provider = await detectEthereumProvider({ silent: true })
       console.log(provider)
       setHasProvider(Boolean(provider)) // transform provider to true or false
-    }
+      if (provider) {                                           /* New */
+      const accounts = await window.ethereum.request(         /* New */
+        { method: 'eth_accounts' }                            /* New */
+      )                                                       /* New */
+      refreshAccounts(accounts)                               /* New */
+      window.ethereum.on('accountsChanged', refreshAccounts)  /* New */
+    } 
+}
 
     getProvider()
+    return () => {                                              /* New */
+      window.ethereum?.removeListener('accountsChanged', refreshAccounts)
+    }
   }, [])
 
-   const updateWallet = async (accounts:any) => {     /* New */
-    setWallet({ accounts })                          /* New */
-  }                                                  /* New */
+   const updateWallet = async (accounts:any) => {     
+    setWallet({ accounts })                          
+  }                                                  
 
-  const handleConnect = async () => {                /* New */
-    let accounts = await window.ethereum.request({   /* New */
-      method: "eth_requestAccounts",                 /* New */
-    })                                               /* New */
-    updateWallet(accounts)                           /* New */
-  }                                                  /* New */
+  const handleConnect = async () => {                
+    let accounts = await window.ethereum.request({   
+      method: "eth_requestAccounts",                 
+    })                                               
+    updateWallet(accounts)                           
+  }                                                  
 
 
     return (
@@ -54,10 +73,12 @@ function StartPage () {
                         </li>
                     </ul> 
                 </div>
-                <button onClick={handleConnect}>Connect MetaMask</button>
+                { window.ethereum?.isMetaMask && wallet.accounts.length < 1 &&  /* Updated */
+        <button class="button-86" role="button" onClick={handleConnect}>Connect MetaMask Wallet</button>
+      }
 
-                <button class="button-86" role="button" onClick={handleConnect}>Connect MetaMask Wallet</button>
-                { wallet.accounts.length > 0 &&                /* New */
+                
+                { wallet.accounts.length > 0 &&                
         <div>Wallet Accounts: { wallet.accounts[0] }</div> //format for later
       }
             
