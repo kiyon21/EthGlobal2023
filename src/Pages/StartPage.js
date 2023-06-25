d2e6b03a123e2a7a2b06019fa84441a8149e4e85
Mena Azab <menaazab@hotmail.com>
2023-06-24 21:18:52 -0400

    fixed error handling

--- src/Pages/StartPage.js
+++ src/Pages/StartPage.js
@@ -9,7 +9,11 @@ import detectEthereumProvider from '@metamask/detect-provider'
 function StartPage () {
     const [hasProvider, setHasProvider] = useState(null)
     const initialState = { accounts: [] }               
-    const [wallet, setWallet] = useState(initialState)  
+    const [wallet, setWallet] = useState(initialState) 
+    
+    const [isConnecting, setIsConnecting] = useState(false)  /* New */
+    const [error, setError] = useState(false)                /* New */
+    const [errorMessage, setErrorMessage] = useState("")     /* New */
 
   useEffect(() => {
     const refreshAccounts = (accounts: any) => {                /* New */
@@ -44,12 +48,24 @@ function StartPage () {
     setWallet({ accounts })                          
   }                                                  
 
-  const handleConnect = async () => {                
-    let accounts = await window.ethereum.request({   
-      method: "eth_requestAccounts",                 
-    })                                               
-    updateWallet(accounts)                           
-  }                                                  
+  const handleConnect = async () => {                   /* Updated */
+  setIsConnecting(true)                               /* New */
+  await window.ethereum.request({                     /* Updated */
+    method: "eth_requestAccounts",
+  })
+  .then((accounts:[]) => {                            /* New */
+    setError(false)                                   /* New */
+    updateWallet(accounts)                            /* New */
+  })                                                  /* New */
+  .catch((err:any) => {                               /* New */
+    setError(true)                                    /* New */
+    setErrorMessage(err.message)                      /* New */
+  })                                                  /* New */
+  setIsConnecting(false)                              /* New */
+}
+
+const disableConnect = Boolean(wallet) && isConnecting
+
 
 
     return (
@@ -74,12 +90,18 @@ function StartPage () {
                     </ul> 
                 </div>
                 { window.ethereum?.isMetaMask && wallet.accounts.length < 1 &&  /* Updated */
-        <button class="button-86" role="button" onClick={handleConnect}>Connect MetaMask Wallet</button>
+        <button class="button-86" role="button" disabled={disableConnect} onClick={handleConnect}>Connect MetaMask Wallet</button>
       }
 
                 
                 { wallet.accounts.length > 0 &&                
         <div className="accountId">Wallet Accounts: { wallet.accounts[0] }</div> //format for later
+      }
+      { error && (                                        /* New code block */
+          <div onClick={() => setError(false)}>
+            <strong>Error:</strong> {errorMessage}
+          </div>
+        )
       }
             
             </div>
