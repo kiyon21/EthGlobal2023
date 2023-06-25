fca30a97e5e5a8e48b7636238a809836b7ba7438
Mena Azab <menaazab@hotmail.com>
2023-06-24 21:16:17 -0400

    qr code working.

--- src/Pages/SuccessPage.js
+++ src/Pages/SuccessPage.js
@@ -1,15 +1,27 @@
 import React from "react";
 import "./SuccessPage.css";
 
+import { useState } from 'react';
+import QRCode from 'react-qr-code';
+import { useSearchParams } from 'react-router-dom';
+
+
 function SuccessPage () {
+    const queryParams = new URLSearchParams(window.location.search)
+    const walletAddress = queryParams.get("wallet")
+    const moneyAmount = queryParams.get("amount")
+
+    let turnQR = "mena"
+
     return (
         
         <section className="SuccessPage">
         <div className="success">
             
             <div className="col-1">
-                <p>Lorem Ipsum
-                </p>
+            <div style={{ background: 'white', padding: '16px' }}>
+                <QRCode size={256} value={turnQR} />
+                </div>
             </div>
 
             <div className="col-2">
